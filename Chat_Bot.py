235f2452baea2b84401baa88a54219a3f2a7801d
NoNamesUserName <46601892+NoNamesUserName@users.noreply.github.com>
2019-04-03 08:09:36 -0500

    Update Chat_Bot.py

--- Chat_Bot.py
+++ Chat_Bot.py
@@ -1,15 +1,13 @@
+import random
 def Greeting ():
-    def check_for_greeting(sentence):
-        for word in sentence.words:
-            if word.lower() in GREETING_KEYWORDS:
-                print(GREETING_RESPONSES)
-                return random.choice(GREETING_RESPONSES)
-    def greetkey():    
-        sentence= str(input("Greeting You Must Enter: "))
+    def greetkey():
+        sentence = input("Greeting You Must Enter: ")
         GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up")
-        GREETING_RESPONSES = ["*Vulcan Salute*", "take me to your leader","greetings earthling", "Nano Nano", "⊑⟒⌰⌰⍜"]
-        print(check_for_greeting(sentence))
+        GREETING_RESPONSES = ["take me to your leader","greetings earthling", "Nano Nano", "⊑⟒⌰⌰⍜"]
+        if sentence in GREETING_KEYWORDS:
+            print(random.choice(GREETING_RESPONSES))
     greetkey()
-    
+
 Greeting()
 
+input()
