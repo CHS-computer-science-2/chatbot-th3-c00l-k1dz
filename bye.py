2895d9f64bae4f8adfe2b6ffed6c6e3da8b9f87e
Mark Hannah <mhannah0225@myduneland.org>
2019-04-05 08:04:05 -0500

    aye

--- bye.py
+++ bye.py
@@ -1,1 +1,11 @@
-
+import random
+def farewell ():
+    def bye():
+        sentence = input()
+        FAREWELL_KEYWORDS = ("bye", "goodbye", "farewell", "see you", "see ya")
+        FAREWELL_RESPONSES = ["peace","farewell earthling", "bye", "☌⍜⍜⎅⏚⊬⟒"]
+        if sentence in FAREWELL_KEYWORDS:
+            print(random.choice(FAREWELL_RESPONSES))
+    bye()
+farewell()
+input()
