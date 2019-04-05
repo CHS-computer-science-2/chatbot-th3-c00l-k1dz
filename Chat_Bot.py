1855538589aac1eb2f625b8bfdff279594b4eed4
JakeT <tselking0197@myduneland.org>
2019-04-05 06:59:10 -0700

    Main

--- Chat_Bot.py
+++ Chat_Bot.py
@@ -1,13 +1,9 @@
 import random
-def Greeting ():
-    def greetkey():
-        sentence = input("Greeting You Must Enter: ")
-        GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up")
-        GREETING_RESPONSES = ["take me to your leader","greetings earthling", "Nano Nano", "⊑⟒⌰⌰⍜"]
-        if sentence in GREETING_KEYWORDS:
-            print(random.choice(GREETING_RESPONSES))
-    greetkey()
-
-Greeting()
-
-input(
+def main():
+    GREETING_RESPONSES = ["take me to your leader","greetings earthling", "Nano Nano", "⊑⟒⌰⌰⍜"]
+    print(random.choice(GREETING_RESPONSES))
+    # all checks go here \/
+    x=0
+    while x==0:
+        input("Type here you must: ")
+main()
