0b757e5d69f5fce91a8f14dc7948a29b71733630
Mark Hannah <mhannah0225@myduneland.org>
2019-04-09 08:22:05 -0500

    main

--- Chat_Bot.py
+++ Chat_Bot.py
@@ -1,22 +1,29 @@
 import random
 
-def Greeting ():
-    def greetkey():
-        sentence = input("Greeting You Must Enter: ")
-        GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up")
-        GREETING_RESPONSES = ["take me to your leader","greetings earthling", "Nano Nano", "⊑⟒⌰⌰⍜"]
-        if sentence in GREETING_KEYWORDS:
-            print(random.choice(GREETING_RESPONSES))
-    greetkey()
-Greeting()
+def greeting(sentence):
+    GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up")
+    GREETING_RESPONSES = ["take me to your leader","greetings earthling", "Nano Nano", "⊑⟒⌰⌰⍜"]
+    if sentence in GREETING_KEYWORDS:
+        print(random.choice(GREETING_RESPONSES))
+    return
+
+def farewell(sentence):
+    FAREWELL_KEYWORDS = ("bye", "goodbye", "farewell", "see you", "see ya")
+    FAREWELL_RESPONSES = ["peace","farewell earthling", "bye", "☌⍜⍜⎅⏚⊬⟒"]
+    if sentence in FAREWELL_KEYWORDS:
+        print(random.choice(FAREWELL_RESPONSES))
+    return
 
 def main():
-    GREETING_RESPONSES = ["take me to your leader","greetings earthling", "Nano Nano", "⊑⟒⌰⌰⍜"]
-    FAREWELL_RESPONSES = ["peace","farewell earthling", "bye", "☌⍜⍜⎅⏚⊬⟒"]
-    print(random.choice(GREETING_RESPONSES))
+    GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up")
+    FAREWELL_KEYWORDS = ("bye", "goodbye", "farewell", "see you", "see ya")
     # all checks go here \/
     x=0
     while x==0:
-        input("Type here you must: ")
+        word = input("Type here you must: ")
+        if word in GREETING_KEYWORDS:
+            greeting(word)
+        elif word in FAREWELL_KEYWORDS:
+            farewell(word)
 
 main()
